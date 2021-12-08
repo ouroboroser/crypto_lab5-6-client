72fa30de9b0f597b7ca6cf0d999dd17bb706d200
ouroboroser <evgeniyagorshchyk@gmail.com>
2021-12-08 16:22:41 +0200

    add display data using kms

--- src/components/UsersData.js
+++ src/components/UsersData.js
@@ -5,6 +5,7 @@ import './forms.css';
 
 export const UsersData = () => {
     const [users, setUsers] = useState([]);
+    const [usersKMS, setUsersKMS] = useState([]);
 
     useEffect(() => {
         const apiUrl = 'http://localhost:3001/users/data/list';
@@ -12,7 +13,9 @@ export const UsersData = () => {
         axios
         .get(apiUrl)
         .then((response) => {
-            setUsers(response.data);
+            setUsers(response.data.data);
+            setUsersKMS(response.data.dataFromKMS);
+            console.log(response.data);
         })
         .catch((e) => {
             console.log(e);
@@ -24,6 +27,19 @@ export const UsersData = () => {
             {users.map(user => {
                 return(
                     <div className = 'user'>
+                        <h3> Data using salsa </h3>
+                        <p> <span className = 'userTitle'> id: </span> <span className = 'userData'> {user.id} </span> </p>
+                        <p> <span className = 'userTitle'> username: </span> <span className = 'userData'> {user.username} </span> </p>
+                        <p> <span className = 'userTitle'> phone: </span> <span className = 'userData'> {user.phone} </span> </p>
+                        <p> <span className = 'userTitle'> address: </span> <span className = 'userData'> {user.address} </span> </p>
+                        <p> <span className = 'userTitle'> email: </span> <span className = 'userData'> {user.email} </span> </p>
+                    </div>
+                )
+            })}
+            {usersKMS.map(user => {
+                return(
+                    <div className = 'user'>
+                        <h3> Data using KMS </h3>
                         <p> <span className = 'userTitle'> id: </span> <span className = 'userData'> {user.id} </span> </p>
                         <p> <span className = 'userTitle'> username: </span> <span className = 'userData'> {user.username} </span> </p>
                         <p> <span className = 'userTitle'> phone: </span> <span className = 'userData'> {user.phone} </span> </p>
